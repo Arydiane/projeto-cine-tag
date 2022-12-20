d4ee2a2f077d4ac14e787d2c795c0e4bc52cad7f
Arydiane <arydianejardim@gmail.com>
2022-12-20 20:55:02 -0300

    adiciona pagina de erro não encontrada

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,12 +3,17 @@ import Titulo from "components/Titulo";
 import { useParams } from "react-router-dom";
 import styles from "./Player.module.css"
 import videos from "json/db.json"
+import NaoEncontrada from "pages/NaoEncontrada";
 
 function Player() {
     const paramentros = useParams();
     const video = videos.find((video) => {
         return video.id === Number(paramentros.id);
     })
+
+    if (!video){
+        return <NaoEncontrada />
+    }
 
     return (
         <>
