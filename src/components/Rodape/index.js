5d34e4fa4996ad1f83449b62a89b0996e41098af
Arydiane <arydianejardim@gmail.com>
2022-12-20 17:33:46 -0300

    adiciona componente rodapé

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from "./Rodape.module.css"
+
+function Rodape(){
+    return (
+        <footer className={styles.rodape}>
+            <h3>Desenvolvido por Arydiane Jardim <br/> no curso React JS da Alura.</h3>
+        </footer>
+    )   
+}
+
+export default Rodape; 
